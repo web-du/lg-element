4ce1209350af53573de5c6668697bd0cfd95f8e0
duwenzheng <1605993932@qq.com>
2021-07-28 17:32:26 +0800

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../packages/**/*.stories.mdx",
+    "../packages/**/*.stories.@(js|jsx|ts|tsx)",
+    "../packages/**/*.stories.js"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
